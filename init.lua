5d078034577480aad062cdb76048c6a1f1de62a7
AlexanderWedekind <awedwork@gmail.com>
2026-01-29 09:43:59 +0000

    removed telescope

--- init.lua
+++ init.lua
@@ -100,22 +100,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
-        {
-            'nvim-telescope/telescope.nvim',
-            dependencies = {
-                "nvim-lua/plenary.nvim",
-                { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-            }
-        }
     },
     install = { colorscheme = { "habamax" } },
     checker = { enabled = true },
 })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>m', ':messages<CR>')
 
