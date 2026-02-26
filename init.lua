c0841c740ef8f041d2e9fa94c4e8cdc040acf483
AlexanderWedekind <awedwork@gmail.com>
2026-02-26 12:30:25 +0000

    can't remember what I did here, committing a few days after making the modification

--- init.lua
+++ init.lua
@@ -44,6 +44,12 @@ vim.keymap.set('n', '<leader>b', ':ls<CR>')
 vim.keymap.set('n', '<C-->', '<C-w>-')
 vim.keymap.set('n', '<C-+>', '<C-w>+')
 
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking (copying) text',
+--   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+--   callback = function() vim.hl.on_yank() end,
+-- })
+
 vim.keymap.set('n', '<leader>d', function()
     local bufnr = vim.api.nvim_get_current_buf()
     local attached = false
@@ -302,8 +308,20 @@ vim.api.nvim_create_autocmd("Filetype", {
     callback = function()
         print('starting php-language-server')
         vim.lsp.start({
-            name = 'php-language-server',
-            cmd = {'php ./vendor/felixfbecker/language-server/bin/php-language-server.php'},
+            name = 'phpactor',
+            root_dir = vim.fs.dirname(vim.fs.find({".git", "composer.json"}, { upward = true })[1]),
+            cmd = {
+                'docker',
+                'run',
+                '--rm',
+                '-i',
+                '-v',
+                root_dir .. ':/app',
+                'phpactor-ls',
+                'phpactor',
+                'language-server',
+                '--stdio'
+            }, -- docker build -t phpactor-ls .
         })
     end
 })
